9f9d5f4574c4cbe1d09f8bd2c9b6e438b92048b0
Nataliia5722 <71754022+Nataliia5722@users.noreply.github.com>
2020-12-07 23:33:15 +0200

    Create main.rb

---
+++ First tasks/main.rb
@@ -0,0 +1,17 @@
+#Main tasks
+#task1
+def print_hello()
+    text1 = "Hello "
+    text2 = "world"
+    print text1+text2
+end
+
+print_hello()
+
+#task2
+var = "hello"
+puts var[0]
+puts var[1]
+puts var[4]
+
+#task3
